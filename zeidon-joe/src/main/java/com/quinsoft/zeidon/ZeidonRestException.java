c835e43dbc5c771f43e7b4ca466effc8eb5bfd85
DeegC <deegc@github.com>
2014-04-09 23:05:00 -0400

    Added REST response OI

---
+++ zeidon-joe/src/main/java/com/quinsoft/zeidon/ZeidonRestException.java
@@ -0,0 +1,22 @@
+/**
+ *
+ */
+package com.quinsoft.zeidon;
+
+/**
+ * @author dgc
+ *
+ */
+public class ZeidonRestException extends ZeidonException
+{
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * @param format
+     * @param strings
+     */
+    public ZeidonRestException( String format, Object... strings )
+    {
+        super( format, strings );
+    }
+}
