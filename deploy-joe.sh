4f2fe74daef2d64c066a285d10aab3a1ef51260f
DeegC <deegc@github.com>
2014-12-10 21:44:33 -0500

    Removed snapshot

--- deploy-joe.sh
+++ deploy-joe.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Deploys JOE to the remote OSSHR repos. Exludes the test projects.
-mvn -P sinatra-server,!test-projects install deploy -DskipTests=true
+mvn -P !test-projects,release install deploy -DskipTests=true
