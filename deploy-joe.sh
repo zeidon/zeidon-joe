e312f6510fef6105faa9c346aa9b2d82cf0fdc34
DeegC <deegc@github.com>
2014-12-15 01:25:04 -0500

    Got deploy to work

--- deploy-joe.sh
+++ deploy-joe.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Deploys JOE to the remote OSSHR repos. Exludes the test projects.
-mvn -P !test-projects,release install deploy -DskipTests=true
+mvn -P !test-projects,release install deploy -DskipTests=true | tee /tmp/deploy.log
