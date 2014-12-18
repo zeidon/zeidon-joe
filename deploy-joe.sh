60eb74f735e94ae203aa838d4c7118b7105c0c34
DeegC <deegc@github.com>
2014-12-17 22:56:42 -0500

    Change to deploy

--- deploy-joe.sh
+++ deploy-joe.sh
@@ -3,6 +3,6 @@
 # Deploys JOE to the remote OSSHR repos. Exludes the test projects.
 #mvn -P !test-projects,release install deploy -DskipTests=true | tee /tmp/deploy.log
 
-mvn -P !test-projects install deploy -DskipTests=true | tee /tmp/deploy.log
+mvn -P !test-projects,release install deploy -DskipTests=true | tee /tmp/deploy.log
 
-mvn gpg:sign-and-deploy-file -Dfile=sinatra-server/target/zeidon-sinatra-server-1.0.7.jar -DrepositoryId=ossrh -Durl=https://oss.sonatype.org/service/local/staging/deploy/maven2/ -DpomFile=sinatra-server/target/zeidon-sinatra-server-1.0.7.pom
+#mvn gpg:sign-and-deploy-file -Dfile=sinatra-server/target/zeidon-sinatra-server-1.0.7.jar -DrepositoryId=ossrh -Durl=https://oss.sonatype.org/service/local/staging/deploy/maven2/ -DpomFile=sinatra-server/target/zeidon-sinatra-server-1.0.7.pom
