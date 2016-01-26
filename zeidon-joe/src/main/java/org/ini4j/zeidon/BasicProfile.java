4ed76eb66a8dfcb118aeee7fa866e7902131fbf1
kellysautter <kellysautter@comcast.net>
2016-01-26 13:11:39 -0800

    Changes for compatibility with Java 8

--- zeidon-joe/src/main/java/org/ini4j/zeidon/BasicProfile.java
+++ zeidon-joe/src/main/java/org/ini4j/zeidon/BasicProfile.java
@@ -140,11 +140,19 @@ public class BasicProfile extends CommonMultiMap<String, Profile.Section> implem
         return remove((Object) section.getName());
     }
 
-    @Override public String remove(Object sectionName, Object optionName)
+    @Override public boolean remove(Object sectionName, Object optionName)
     {
         Section sec = get(sectionName);
 
-        return (sec == null) ? null : sec.remove(optionName);
+        if (sec == null || !sec.containsKey(optionName))
+        {
+            return false;
+        }
+        else
+        {
+            sec.remove(optionName);
+            return true;
+        }
     }
 
     boolean isTreeMode()
