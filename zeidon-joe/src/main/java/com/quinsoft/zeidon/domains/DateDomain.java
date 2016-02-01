76759fce3eac10bde094281df863cec627963251
DeegC <dgchristensen@comcast.net>
2016-02-01 08:54:14 -0500

    Made DateDomain a subclass of DateTimeDomain (instead of vice versa)

--- zeidon-joe/src/main/java/com/quinsoft/zeidon/domains/DateDomain.java
+++ zeidon-joe/src/main/java/com/quinsoft/zeidon/domains/DateDomain.java
@@ -19,29 +19,23 @@
 
 package com.quinsoft.zeidon.domains;
 
-import java.util.Date;
 import java.util.Map;
 
-import org.apache.commons.lang3.StringUtils;
 import org.joda.time.DateTime;
 import org.joda.time.format.DateTimeFormat;
 import org.joda.time.format.DateTimeFormatter;
 
 import com.quinsoft.zeidon.Application;
 import com.quinsoft.zeidon.AttributeInstance;
-import com.quinsoft.zeidon.EntityInstance;
-import com.quinsoft.zeidon.InvalidAttributeValueException;
 import com.quinsoft.zeidon.ObjectEngine;
 import com.quinsoft.zeidon.Task;
-import com.quinsoft.zeidon.ZeidonException;
 import com.quinsoft.zeidon.objectdefinition.AttributeDef;
-import com.quinsoft.zeidon.utils.JoeUtils;
 
 /**
  * @author DG
  *
  */
-public class DateDomain extends AbstractDomain
+public class DateDomain extends DateTimeDomain
 {
     protected DateTimeFormatter defaultDateFormatter = DateTimeFormat.forPattern( ObjectEngine.INTERNAL_DATE_STRING_FORMAT );
 
@@ -53,201 +47,19 @@ public class DateDomain extends AbstractDomain
     @Override
     public Object convertExternalValue(Task task, AttributeInstance attributeInstance, AttributeDef attributeDef, String contextName, Object externalValue)
     {
-        // If external value is an AttributeInstance then get *its* internal value.
-        if ( externalValue instanceof AttributeInstance )
-            externalValue = ((AttributeInstance) externalValue).getValue();
+        DateTime dt = (DateTime) super.convertExternalValue( task, attributeInstance, attributeDef, contextName, externalValue );
+        if ( dt != null)
+            dt = dt.withMillisOfDay( 0 );
 
-    	// KJS - Added 01/27/11 because of line 2836 in lTrnscpt_Object.java
-    	// OrderEntityForView( lTrnscpt, "StudentMajorDegreeTrack", "wPrimarySortOrder A GraduationDate A" );
-    	// value = null so we are getting to the exception.  Will try returning null, see what happens.
-    	if ( externalValue == null )
-    		return null;
-
-        if ( externalValue instanceof DateTime )
-            return externalValue;
-
-        if ( externalValue instanceof Date )
-            return new DateTime( externalValue );
-
-        // VML operations use "" as synonymous with null.
-        if ( externalValue instanceof String && StringUtils.isBlank( (String) externalValue ) )
-            return null;
-
-        if ( externalValue instanceof CharSequence )
-        {
-            // If string is "NOW" then we'll use current datetime.
-            if ( externalValue.toString().equals( "NOW" ) )
-                return new DateTime();
-
-            DomainContext context = getContext( task, contextName );
-            return context.convertExternalValue( task, attributeDef, externalValue.toString() );
-        }
-
-        throw new InvalidAttributeValueException( attributeDef, externalValue,
-                                                  "Invalid object: Domain %s cannot convert value for context %s.",
-                                                  this.getClass().getName(), contextName );
-    }
-
-    /**
-     * Attempt to get the context.  This differs from the normal getContext() by attempting to create
-     * a SimpleDateFormatter using the contextName as the format if a context is not found by name.
-     */
-    @Override
-    public DomainContext getContext(Task task, String contextName)
-    {
-        DomainContext context = getContext( contextName );
-        if ( context != null )
-            return context;  // We found one by name.
-
-        if ( StringUtils.isBlank( contextName ) )
-            throw new ZeidonException("Domain '%s' does not have a default context defined.", getName() );
-
-        // Create a temporary new one and set its edit string to the context name.
-        DateContext dateContext = new DateContext( this );
-        dateContext.setName( contextName );
-        dateContext.setEditString( contextName );
-        return dateContext;
-    }
-
-    @Override
-    public void validateInternalValue( Task task, AttributeDef attributeDef, Object internalValue ) throws InvalidAttributeValueException
-    {
-      if ( internalValue instanceof DateTime )
-            return;
-
-        throw new InvalidAttributeValueException( attributeDef, internalValue, "Internal value must be Joda DateTime, not %s",
-                                                  internalValue.getClass().getName() );
+        return dt;
     }
 
     @Override
     public String convertToString(Task task, AttributeDef attributeDef, Object internalValue)
     {
         if ( internalValue == null )
-            return StringDomain.checkNullString( attributeDef.getDomain().getApplication(), null );
-
-    	if ( internalValue.toString().isEmpty())
-    		return internalValue.toString();
+            return super.convertToString( task, attributeDef, internalValue );
 
         return defaultDateFormatter.print( (DateTime) internalValue );
-    }
-
-    /**
-     * Adds milliseconds to the datetime value.
-     */
-    @Override
-    public Object addToAttribute( Task task, AttributeInstance attributeInstance, AttributeDef attributeDef, Object currentValue, Object addValue )
-    {
-        DateTime date1 = (DateTime) convertExternalValue( task, attributeInstance, attributeDef, null, currentValue );
-
-        if ( addValue == null )
-            return date1;
-
-        if ( addValue instanceof Number )
-        {
-            int millis = ((Number) addValue).intValue();
-            return date1.plusMillis( millis );
-        }
-
-        throw new ZeidonException( "Value type of %s not supported for add to DateDomain", addValue.getClass().getName() );
-    }
-
-    @Override
-    public DomainContext newContext(Task task)
-    {
-        return new DateContext( this );
-    }
-
-    /**
-     * Generate a random test value for this domain.  This is used by test code to create random
-     * test data.
-     *
-     * @param task current task
-     * @param attributeDef def of the attribute.
-     * @param entityInstance if not null this is the EntityInstance that will store the test data.
-     *
-     * @return random test data for this domain.
-     */
-    @Override
-    public Object generateRandomTestValue( Task task, AttributeDef attributeDef, EntityInstance entityInstance )
-    {
-        DomainContext context = getDefaultContext();
-        return context.convertToString( task, attributeDef, new DateTime() );
-    }
-
-    private class DateContext extends BaseDomainContext
-    {
-        /**
-         * URL for Java SimpleDateFormat help.
-         */
-        private static final String JAVA_DATE_FORMATTING_URL = "http://docs.oracle.com/javase/6/docs/api/java/text/SimpleDateFormat.html";
-
-        public DateContext(Domain domain)
-        {
-            super( domain );
-        }
-
-        private DateTimeFormatter formatter;
-
-        @Override
-        public String convertToString(Task task, AttributeDef attributeDef, Object internalValue) throws ZeidonException
-        {
-        	if ( internalValue == null )
-        		return StringDomain.checkNullString(task.getApplication(), null);
-
-        	if ( internalValue.toString().isEmpty())
-        		return internalValue.toString();
-
-        	if ( formatter == null )
-        	    throw new ZeidonException( "JavaEditString is not set for context %s", this.toString() );
-
-            return formatter.print( (DateTime) internalValue );
-        }
-
-        /**
-         * Assumes a string.
-         */
-        @Override
-        public Object convertExternalValue(Task task, AttributeDef attributeDef, Object value) throws InvalidAttributeValueException
-        {
-        	if ( value == null )
-        		return null;
-
-            // If external value is an AttributeInstance then get *its* internal value.
-            if ( value instanceof AttributeInstance )
-                value = ((AttributeInstance) value).getValue();
-
-            String s = (String) value;
-
-            // VML operations use "" as synonymous with null.
-        	if ( StringUtils.isBlank( s ) )
-        		return null;
-
-        	// A common internal date/time format is the DefaultTimeFormat.  Let's try
-        	// a quick check to see if 's' is the same length and only digits.
-        	if ( s.length() >= ObjectEngine.INTERNAL_DATE_STRING_FORMAT.length() && JoeUtils.onlyDigits( s ) )
-                return JoeUtils.parseStandardDateString( s );
-
-        	if ( formatter == null )
-        	    throw new ZeidonException( "JavaEditString is not set for context %s", this.toString() );
-
-        	try
-        	{
-        	    return formatter.parseDateTime( s );
-        	}
-        	catch ( Exception e )
-        	{
-        	    throw new InvalidAttributeValueException( attributeDef, s, e.getMessage() )
-        	                         .appendMessage( "Format string = %s", getEditString() )
-        	                         .appendMessage( "See %s for help on Java Date formatting", JAVA_DATE_FORMATTING_URL )
-        	                         .setCause( e );
-        	}
-        }
-
-        @Override
-        protected void setEditString( String editString )
-        {
-            super.setEditString( editString );
-            formatter = JoeUtils.createDateFormatterFromEditString( editString );
-        }
     }
 }
