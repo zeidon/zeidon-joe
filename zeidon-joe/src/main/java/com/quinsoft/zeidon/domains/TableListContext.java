e78d905c723716d1bc73c228e21e3ac111d9191f
kellysautter <kellysautter@comcast.net>
2017-11-02 12:01:18 -0500

    Reverse DayOfWeek language issue which we fixed in local domain

--- zeidon-joe/src/main/java/com/quinsoft/zeidon/domains/TableListContext.java
+++ zeidon-joe/src/main/java/com/quinsoft/zeidon/domains/TableListContext.java
@@ -46,14 +46,14 @@ public class TableListContext extends BaseDomainContext implements TableDomainCo
     /**
      * Map of the internal value to TableEntry.
      */
-    private Map<String, TableEntry> internalMap = new HashMap<String, TableEntry>();
-    private Map<String, TableEntry> externalMap = new HashMap<String, TableEntry>();
-    private List<TableEntry>        entryList = new ArrayList<TableEntry>();
+    protected Map<String, TableEntry> internalMap = new HashMap<String, TableEntry>();
+    protected Map<String, TableEntry> externalMap = new HashMap<String, TableEntry>();
+    protected List<TableEntry>        entryList = new ArrayList<TableEntry>();
 
     /**
      * We use a string domain to convert external values to a string.
      */
-    private StringDomain            stringConverter;
+    protected StringDomain            stringConverter;
 
     private static final Map<String, Object> STRING_CONVERTER_PROPERTIES =
         Collections.unmodifiableMap( new HashMap<String, Object>() {
@@ -155,8 +155,8 @@ public class TableListContext extends BaseDomainContext implements TableDomainCo
         // additionally loaded the dynamic values from the database (if it's a dynamic domain). But there is an issues we
         // run into with language conversion and SetAttrFromStrByContext with DayOfWeek so for the moment I am commenting them
         // out until we figure out what to do with DayOfWeek.
-        //internalMap = new HashMap<String, TableEntry>();
-        //externalMap = new HashMap<String, TableEntry>();
+        internalMap = new HashMap<String, TableEntry>();
+        externalMap = new HashMap<String, TableEntry>();
     }
 
     @Override
