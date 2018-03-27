b55bed0dd26f880a2c0e7a773d9a4ac74a22a9b7
kellysautter <kellysautter@comcast.net>
2018-03-26 22:30:12 -0400

    added perygreneTest

--- zeidon-tests/src/test/java/com/quinsoft/zeidon/test/TestPerygrene.java
+++ zeidon-tests/src/test/java/com/quinsoft/zeidon/test/TestPerygrene.java
@@ -14,7 +14,7 @@ import com.quinsoft.zeidon.Task;
 import com.quinsoft.zeidon.View;
 import com.quinsoft.zeidon.standardoe.JavaObjectEngine;
 //import com.quinsoft.zeidon.test.TestSWAU.SwauVmlTester;
-//import com.quinsoft.zeidon.test.TestPerygrene.VmlTester;
+//import com.quinsoft.zeidon.test.TestCheetah2.VmlTester;
 import com.quinsoft.zeidon.vml.VmlObjectOperations;
 import com.quinsoft.zeidon.vml.zVIEW;
 
@@ -51,35 +51,35 @@ public class TestPerygrene
 	{
 	    View         testview;
 		testview = perygrene.activateEmptyObjectInstance( "mDrvShiftRoutes" );
-		perygreneVmlTester tester = new perygreneVmlTester( testview );
+		PerygreneVmlTester tester = new PerygreneVmlTester( testview );
 		tester.testActivateFile( testview );
         System.out.println("===== Finished testOrderEntity ========");
 	}
 
-	private class perygreneVmlTester extends VmlObjectOperations
+
+	private class PerygreneVmlTester extends VmlObjectOperations
 	{
-		public perygreneVmlTester( View view )
+		public PerygreneVmlTester( View view )
 		{
 			super( view );
 		}
 
         public int
-        testActivateFile( View ViewToWindow)
+        testActivateFile( View     ViewToWindow)
         {
-        	zVIEW    mDrvShiftRoutes = new zVIEW( );
-        	int      RESULT = 0;
+        	   zVIEW    mDrvShiftRoutes = new zVIEW( );
+        	   int      RESULT = 0;
 
         	 View tmpview = ViewToWindow.getTask().deserializeOi()
-        	 .fromFile( "target/test-classes/testdata//perygrene/drvshiftbeforedeleting.por" )
+        	 .fromFile( "target/test-classes/testdata//Perygrene/drvshiftbeforedeleting.por" )
         	 .setLodDef( "mDrvShiftRoutes" )
         	 //.setFlags(ActivateFlags.IGNORE_ENTITY_ERRORS)
         	 .activateFirst();
         	 mDrvShiftRoutes.setView(tmpview); 
 
-        	 //:ActivateOI_FromFile( mDrvShiftRoutes, "mDrvShiftRoutes", ViewToWindow, "c:/temp/mDrvShiftRoutes.por", zSINGLE )
-        	ActivateOI_FromFile( mDrvShiftRoutes, "mDrvShiftRoutes", ViewToWindow, "c:/temp/drvshiftbeforedeleting.por", zSINGLE );
-			
-        	return 0;
+
+        	   return 0;
        }
-   }
+
+     }
 }
