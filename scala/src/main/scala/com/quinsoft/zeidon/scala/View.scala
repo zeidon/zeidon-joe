49499e61d567cb5794f4e374b8d9ed9f97f5fc12
DeegC <dgchristensen@comcast.net>
2020-06-22 19:13:10 -0400

    Make it easier to return a DynamicTask from a scala view

--- scala/src/main/scala/com/quinsoft/zeidon/scala/View.scala
+++ scala/src/main/scala/com/quinsoft/zeidon/scala/View.scala
@@ -36,7 +36,7 @@ import com.quinsoft.zeidon.standardoe.IteratorBuilder
  * A Scala wrapper for the JOE View.  This object uses dynamic methods that allows
  * users to write code using VML-like view.entity.attribute syntax.
  */
-class View( val task: Task ) extends Dynamic {
+class View( val jtask: Task ) extends Dynamic {
 
     /**
      * The underlying EntityDef for this View.  May be null.
@@ -47,6 +47,8 @@ class View( val task: Task ) extends Dynamic {
      * The underlying Java view for this Scala View.  May be null.
      */
     var jview: com.quinsoft.zeidon.View = null
+
+    val task = new DynamicTask( jtask )
 
     def this( jv: com.quinsoft.zeidon.View ) = {
         this( jv.getTask() )
@@ -80,9 +82,9 @@ class View( val task: Task ) extends Dynamic {
 
     private def setLod( lodName: String, appName: String = null ): View = {
         if ( appName == null )
-            jlodDef = task.getApplication().getLodDef( task, lodName )
+            jlodDef = jtask.getApplication().getLodDef( jtask, lodName )
         else
-            jlodDef = task.getApplication( appName ).getLodDef( task, lodName )
+            jlodDef = jtask.getApplication( appName ).getLodDef( jtask, lodName )
 
         if ( jview != null && jview.getLodDef() != jlodDef )
             throw new ZeidonException( "LodDef set by basedOnLod doesn't match view." )
@@ -136,13 +138,13 @@ class View( val task: Task ) extends Dynamic {
      * Creates an empty OI for this view.  The LOD must have been previously
      * specified with basedOn.
      * {{{
-     *      val mUser = new View( task ) basedOn "mUser"
+     *      val mUser = new View( jtask ) basedOn "mUser"
      *      mUser activateEmpty()
      * }}}
      */
     def activateEmpty() = {
         validateLodDef
-        jview = task.activateEmptyObjectInstance( jlodDef )
+        jview = jtask.activateEmptyObjectInstance( jlodDef )
         this
     }
 
@@ -227,7 +229,7 @@ class View( val task: Task ) extends Dynamic {
     /**
      * Creates a new view, potentially with a different owning task.
      */
-    def newView( task: Task = this.task ) = { validateNonNull; new View( task ).from( this ) }
+    def newView( task: Task = this.jtask ) = { validateNonNull; new View( task ).from( this ) }
 
     /**
      * Sets the name of this view in its owning task.
@@ -422,7 +424,7 @@ class View( val task: Task ) extends Dynamic {
         try {
             validateNonNull
 
-            val oe = task.getObjectEngine
+            val oe = jtask.getObjectEngine
             var operMap = oe.getCacheMap().getOrCreate( classOf[ObjectOperationMap] )
             val oper = operMap.getObjectOperation( operationName, jlodDef, args: _* )
             val value = oper.invokeOperation( this, args: _* )
