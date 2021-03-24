abda93088fe56b206736c122daad84c59db3181e
kellysautter <kellysautter@comcast.net>
2021-03-23 20:44:51 -0400

    Added new test mFAProfTemporalFinAidAward

--- zeidon-tests/src/test/java/com/quinsoft/zeidon/test/TestZencas.java
+++ zeidon-tests/src/test/java/com/quinsoft/zeidon/test/TestZencas.java
@@ -1370,9 +1370,19 @@ public class TestZencas
 		testview = zencas.activateEmptyObjectInstance( "mFASrc" );
 		VmlTester tester = new VmlTester( testview );
 		tester.mFAProfTemporalPerProfileFinAidAwardPeriodPathTest( testview );
-        System.out.println("===== Finished mFAProfTemporalIssue3 ========");
+        System.out.println("===== Finished mFAProfTemporalPerProfileFinAidAwardPeriodPathTest ========");
 	}
-//mFAProfTemporalTest
+
+	@Test
+	public void mFAProfTemporalFinAidAward()
+	{
+	    View         testview;
+		testview = zencas.activateEmptyObjectInstance( "mFASrc" );
+		VmlTester tester = new VmlTester( testview );
+		tester.mFAProfTemporalFinAidAward( testview );
+        System.out.println("===== Finished mFAProfTemporalFinAidAward ========");
+	}
+
 	@Test
 	public void mFAProfTemporalLinkIssue()
 	{
@@ -1991,6 +2001,63 @@ public class TestZencas
 		   return 0;
 		}
 
+
+		public int
+		mFAProfTemporalFinAidAward( View ViewToWindow )
+		{
+		   zVIEW    mPerson = new zVIEW( );
+		   zVIEW    mFAProf = new zVIEW( );
+		   zVIEW    mFASrc = new zVIEW( );
+		   zVIEW    lTermLST = new zVIEW( );
+		   zVIEW    wXferO = new zVIEW( );
+		   zVIEW    vTempViewVar_0 = new zVIEW( );
+		   int RESULT=0;
+
+		   // KJS 03/23/21 
+		   // but when that test gets fixed, I want to make sure that the values in PerPeriodFinAidAwardDisbursement, down the
+		   // PerProfileFinAidAwardPeriod path show the same values as the original FinAidAwardDisbursement entity under FinAidAward.
+		   // The PerProfileFinAidAwardPeriod path is the "included" path.
+
+		    // Set up code.
+		    RESULT = ActivateEmptyObjectInstance( wXferO, "wXferO", ViewToWindow, zSINGLE );
+		    RESULT = CreateEntity( wXferO, "Root", zPOS_AFTER );
+		    SetNameForView( wXferO, "wXferO", null, zLEVEL_TASK );
+		    fnLocalBuildlTermLST( ViewToWindow, vTempViewVar_0 );
+			RESULT = ActivateObjectInstance( lTermLST, "lTermLST", ViewToWindow, vTempViewVar_0, zMULTIPLE );
+			DropView( vTempViewVar_0 );
+			SetNameForView( lTermLST, "lTermLST", null, zLEVEL_TASK );
+			OrderEntityForView( lTermLST, "CollegeTerm", "CollegeYear.Year D CollegeTerm.Semester D" );
+
+		   o_fnLocalBuildQualmPerson( ViewToWindow, vTempViewVar_0, 18808 );
+		   RESULT = ActivateObjectInstance( mPerson, "mPerson", ViewToWindow, vTempViewVar_0, zSINGLE );
+		   DropView( vTempViewVar_0 );
+
+
+		   o_fnLocalBuildQualmFASrc( ViewToWindow, vTempViewVar_0, 348 );
+		   RESULT = ActivateObjectInstance( mFASrc, "mFASrc", ViewToWindow, vTempViewVar_0, zACTIVATE_ROOTONLY );
+		   DropView( vTempViewVar_0 );
+	       SetNameForView( mFASrc, "mFASrc", null, zLEVEL_TASK );
+
+		    RESULT = ActivateEmptyObjectInstance( mFAProf, "mFAProf", ViewToWindow, zSINGLE );
+		    SetNameForView( mFAProf, "mFAProf", null, zLEVEL_TASK );
+		    RESULT = CreateEntity( mFAProf, "FinAidProfile", zPOS_AFTER );
+			RESULT = IncludeSubobjectFromSubobject( mFAProf, "Person", mPerson, "Person", zPOS_AFTER );
+
+			CreateTemporalEntity( mFAProf, "FinAidAward", zPOS_AFTER );
+		    mFAProf.cursor("FinAidAward").getAttribute("AwardType").setValue("G");
+		    mFAProf.cursor("FinAidAward").getAttribute("AwardStatus").setValue("A");
+			RESULT = IncludeSubobjectFromSubobject( mFAProf, "FinAidSource", mFASrc, "FinAidSource", zPOS_AFTER );
+
+		    RESULT = CreateEntity( mFAProf, "FinAidAwardDisbursement", zPOS_AFTER );
+		    mFAProf.cursor("FinAidAwardDisbursement").getAttribute("Amount").setValue( 500) ;
+		    mFAProf.cursor("FinAidAwardDisbursement").getAttribute("AmountExpected").setValue( 500) ;
+		    mFAProf.cursor("FinAidAward").acceptSubobject();
+	        Assert.assertTrue( "FinAidSource entity exists but is empty after acceptSubobject", mFAProf.cursor("FinAidSource").getAttribute("ID").getString().length() > 0 );
+
+		    return 0;
+ 		 
+		}
+		
 		public int
 		mFAProfTemporalLinkIssue( View ViewToWindow )
 		{
