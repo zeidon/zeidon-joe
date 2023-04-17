0faa69f1fd4b3fa2e60d43e64ce647e060fdd1d0
kellysautter <kellysautter@comcast.net>
2023-04-17 17:49:42 -0400

    commented out some lines in the test

--- zeidon-tests/src/test/java/com/quinsoft/zeidon/test/TestMMCRM.java
+++ zeidon-tests/src/test/java/com/quinsoft/zeidon/test/TestMMCRM.java
@@ -80,7 +80,7 @@ public class TestMMCRM
 		   zVIEW    mPayroll2 = new zVIEW( );
 		   int      RESULT = 0;
 
-
+		   /* We don't really need to working sample...
 		   // THIS SAMPLE IS WORKING
 		   //:ACTIVATE mOrganization EMPTY
 		   RESULT = ActivateEmptyObjectInstance( mOrganization, "mOrganization", ViewToWindow, zSINGLE );
@@ -99,8 +99,7 @@ public class TestMMCRM
 		   SetNameForView( mPayroll, "mPayroll", null, zLEVEL_TASK );
 
 		   RESULT = IncludeSubobjectFromSubobject( mPayroll, "Organization", mOrganization, "Organization", zPOS_AFTER );
-		   //:   
-		   //:// 
+		   */
 
 		   // THIS SAMPLE WITH TEMPORAL NOT WORKING
 		   //:ACTIVATE mOrganization2 EMPTY
@@ -123,9 +122,6 @@ public class TestMMCRM
 
 		   RESULT = IncludeSubobjectFromSubobject( mPayroll2, "Organization", mOrganization2, "Organization", zPOS_AFTER );
 		   return( 0 );
-//		       
-		//    
-		// END
 		} 
 
    }
