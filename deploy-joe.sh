c465eda32b5b8c71091cef70baaf68b18843fc3f
dgc <dgc@dgchristensen.net>
2026-07-20 00:14:53 -0400

    Updates to pom files to deploy to sonatype nexus repo

--- deploy-joe.sh
+++ deploy-joe.sh
@@ -3,11 +3,11 @@
 # Deploys JOE to the remote OSSHR repos. Exludes the test projects.
 if [[ "$1" == "release" ]]; then
     mvn -P !test-projects,deploy clean install deploy -DskipTests=true | tee /tmp/deploy.log
-    echo "https://oss.sonatype.org/#nexus-search;quick~zeidon-joe"
-    echo "https://oss.sonatype.org/service/local/repositories/comquinsoft-1051/content/com/quinsoft/zeidon/zeidon-joe/"
+    echo "https://central.sonatype.com/publishing/deployments"
+    echo "https://central.sonatype.com/artifact/com.quinsoft.zeidon/zeidon-joe"
 else
     mvn -P !test-projects         clean install deploy -DskipTests=true | tee /tmp/deploy.log
-    echo "https://oss.sonatype.org/content/repositories/snapshots/com/quinsoft/zeidon/zeidon-joe/"
+    echo "https://central.sonatype.com/repository/maven-snapshots/com/quinsoft/zeidon/zeidon-joe/"
 fi
 
-#mvn gpg:sign-and-deploy-file -Dfile=sinatra-server/target/zeidon-sinatra-server-1.3.9.jar -DrepositoryId=ossrh -Durl=https://oss.sonatype.org/service/local/staging/deploy/maven2/ -DpomFile=sinatra-server/target/zeidon-sinatra-server-1.3.9.pom
+#mvn gpg:sign-and-deploy-file -Dfile=sinatra-server/target/zeidon-sinatra-server-1.3.9.jar -DrepositoryId=central -Durl=https://central.sonatype.com/repository/maven-snapshots/ -DpomFile=sinatra-server/target/zeidon-sinatra-server-1.3.9.pom
